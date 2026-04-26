189fc22306778c35e7b56402fb19b595a3d0b2b9
revianggita <revianggita13@gmail.com>
2026-04-26 09:22:54 +0700

    initial commit - absensi kegiatan system

---
+++ database/migrations/2026_04_26_015941_create_kegiatan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kegiatan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kegiatan');
+            $table->date('tanggal_kegiatan');
+            $table->time('waktu_mulai');
+            $table->string('token_link')->unique();
+            $table->string('status'); // open / closed
+
+            $table->foreignId('created_by')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kegiatan');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test-db', function () {
+    return DB::connection('oracle')->select('SELECT * FROM dual');
+});
